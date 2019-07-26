40e48b53dfe1beae58cb5dcc89e85711c7eb8e05
jk25084 <aichirenmaoji@163.com>
2019-07-26 16:09:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var app = tcb.init({
 var auth = tcb.auth({persistence: 'session'})
 auth.weixinAuthProvider({
     appid: 'wx8b0f73bdf6343e00',
-    scope: 'snsapi_base'
+    scope: 'snsapi_userinfo'
 }).signIn()
 .then(res=> app.callFunction({ name: 'access', data: { }}))
 .catch(console.error);

e4250ef7c207e5f84bd73c6c08f261ce9d28df58
jk25084 <aichirenmaoji@163.com>
2019-07-26 13:19:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ var app = tcb.init({
     env: 'moxigan'
   })
 
+var auth = app.auth()
+auth.weixinAuthProvider({
+    appid: 'wx8b0f73bdf6343e00',  //微信应用appid
+    scope: 'snsapi_base'     //网页授权类型
+}).signIn((err, res) => {
+if(res)console.log(res);if(err)console.error(err);
+})
+
 app.callFunction({ name: 'access', data: { hello: 'world' }, function(err, res) {
 if(res)console.log(res);if(err)console.error(err);
 }});

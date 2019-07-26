e7207e75651546bdcbbbf86a6c868b047d6fcf9d
jk25084 <aichirenmaoji@163.com>
2019-07-26 12:50:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ var app = tcb.init({
     env: 'moxigan'
   })
 
-app.callFunction({
-  // 云函数名称
-  name: 'access',
-  // 传给云函数的参数
-  data: {
-    a: 1
-  },
-})
+app.callFunction({name: 'access',data: {a: 1}})
 .then(console.log)
 .catch(console.error)

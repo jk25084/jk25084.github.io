b08ddd5588bae848b6751d41651859710db10425
jk25084 <aichirenmaoji@163.com>
2019-07-26 14:03:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,7 @@ var auth = app.auth()
 auth.weixinAuthProvider({
     appid: 'wx8b0f73bdf6343e00',  //微信应用appid
     scope: 'snsapi_base'     //网页授权类型
-}).signIn((err, res) => {
-if(res)console.log(res);if(err)console.error(err);
-})
-
-app.callFunction({ name: 'access', data: { hello: 'world' }, function(err, res) {
-if(res)console.log(res);if(err)console.error(err);
-}});
+}).signIn()
+.then(res=> app.callFunction({ name: 'access', data: { hello: 'world' }))
+.then(console.log)
+.catch(console.error);
